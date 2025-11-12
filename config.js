b034e921f44092d91b05bdb0d4fa174151cd4023
juliocafu <55306844+juliocafu@users.noreply.github.com>
2025-11-12 12:07:02 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+
+const encodedConfig = {
+    apiKey: "QUl6YVN5Qjh1ajZKcWRzaFEzVWUtN2FwY0hzVHdjQ2JRcGc2TmtR",  
+    projectId: "YXBpcmlmanVhbg==",
+	messagingSenderId: "NzMxMDQ4ODk4NTgx",
+	appId: "MTo3MzEwNDg4OTg1ODE6d2ViOjM4YzkzMzMzOWNlNjg1OTNkZTVlMWM="
+    // ... el resto de las claves ...
+};
